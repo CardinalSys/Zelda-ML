0ddd7d5933eb2775bbe2d814ceaa476d26ffaeb7
Cardinal <alessandro.benvenuti09@gmail.com>
2023-09-02 02:10:41 -0400

    Read player position and direction

--- ZeldaHook/ZeldaHook/ZeldaHook.cpp
+++ ZeldaHook/ZeldaHook/ZeldaHook.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <stdlib.h>
 #include "proc.h"
 
 
 uintptr_t moduleBase;
 HANDLE hProcess;
+uintptr_t baseAddress;
 
 void HookEmulator();
+void GetVariables();
+
+
+//Variables
+BYTE playerXPos = 0;
+BYTE playerYPos = 0;
+BYTE playerDir = 0;
 
 int main()
 {
@@ -20,10 +29,34 @@ void HookEmulator()
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
         //Get Module base address
-        moduleBase = GetModuleBaseAddress(procId, L"Mesen.exe");
+        moduleBase = GetModuleBaseAddress(procId, L"MesenCore.dll");
         //Get handle to process
         hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+        //Resolve base address
+        baseAddress = moduleBase + 0x042E0F30;
 
-        std::cout << hProcess;
+        while (true)
+        {
+            GetVariables();
+            Sleep(100);
+        }
+        
     }
+}
+
+void GetVariables()
+{
+    std::vector<unsigned int> playerXposOffsets = {0xB8, 0x78, 0x70};
+    uintptr_t playerXposAddress = FindDMAAddy(hProcess, baseAddress, playerXposOffsets);
+    ReadProcessMemory(hProcess, (BYTE*)playerXposAddress, &playerXPos, sizeof(char), nullptr);
+
+    std::vector<unsigned int> playerYposOffsets = { 0xB8, 0x78, 0x84 };
+    uintptr_t playerYposAddress = FindDMAAddy(hProcess, baseAddress, playerYposOffsets);
+    ReadProcessMemory(hProcess, (BYTE*)playerYposAddress, &playerYPos, sizeof(char), nullptr);
+
+    std::vector<unsigned int> playerDirOffsets = { 0xB8, 0x78, 0x98 };
+    uintptr_t playerDirAddress = FindDMAAddy(hProcess, baseAddress, playerDirOffsets);
+    ReadProcessMemory(hProcess, (BYTE*)playerDirAddress, &playerDir, sizeof(char), nullptr);
+
+    std::cout << (int)playerDir << "\n";
 }
